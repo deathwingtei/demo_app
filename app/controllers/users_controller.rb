e6a384bb526af1533cfb0a0080205c55013709ee
Deathwing <deathwing@deathwing-Inspiron-N5010.(none)>
2013-02-05 12:36:29 +0700

    Create show user with id, view all user, and edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
 	def index #first index to show
-
+		@users = User.all
 	end
 
 	def new
@@ -22,11 +22,20 @@ class UsersController < ApplicationController
 	end
 
 	def edit # for edit something
-
+  		@user = User.find(params[:id])
 	end
 
 	def update # for update something
-
+		@user = User.find(params[:id])
+		    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
 	end
 
 	def destroy # for destroy something
